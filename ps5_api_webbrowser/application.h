c5e32f32c4eff40fc5624a837d3312a7ea88230f
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2024-02-18 16:40:39 +0800

    update

--- ps5_api_webbrowser/application.h
+++ ps5_api_webbrowser/application.h
@@ -1,8 +1,3 @@
-﻿/* SIE CONFIDENTIAL
- * PlayStation(R)5 Programmer Tool Runtime Library Release 2.00.00.09-00.00.00.0.1
- * Copyright (C) 2019 Sony Interactive Entertainment Inc.
- */
-
 #pragma once
 
 #include <sampleutil.h>
